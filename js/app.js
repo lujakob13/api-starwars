1aef24f7a39c78a580f3a16a6931506afa0f5128
luca jakobson <luca081104@gmail.com>
2024-08-01 14:59:38 -0300

    cree la funcion para obtener la informacion de las naves

--- js/app.js
+++ js/app.js
@@ -10,14 +10,27 @@ function obtenerpersonajes(){
     try{
         fetch(URLBase + endPointPersonajes)
         .then(response => response.json())
-        .then(data => console.log(data.results))
+        .then(data => createCards(data.results))
     }catch(error){
         console.log("error la obtener informacion sobre personajes")
     }
 }
 
+function obtenernaves(){
+    try{
+        fetch(URLBase + endPointNaves)
+        .then(response => response.json())
+        .then(data => createCards(data.results))
+    }catch(error){
+        console.log("error al obtener informacion sobre las naves")
+    }
+}
 
 
 
+// function createCards(personajes, naves){
+//     containercard.innerHTML = ""
+//     for (let personaje)
+// }
 
 
